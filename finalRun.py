35f19336d13c1e0041eb20cb4d1e02d93359a5ad
laboni68 <labonisarker15@gmail.com>
2021-09-26 18:52:08 -0700

    file name is changed from logicalSummary to findSummary

--- finalRun.py
+++ finalRun.py
@@ -2,8 +2,9 @@ import claripy
 
 x0 = claripy.BVS("x0",64)
 x1 = claripy.BVS("x1",64)
+x2 = claripy.BVS("x2",64)
 
-p=claripy.Not(claripy.Or(claripy.And(claripy.And(x1>0x27,x1>0x31,x1<=0x3b,x0==0x3),claripy.And(x1<=0x3b,x1>0x31,x0==0x3)),claripy.And(claripy.Not(claripy.And(x1>0x27,x1>0x31,x1<=0x3b,x0==0x3)),claripy.Not(claripy.And(x1<=0x3b,x1>0x31,x0==0x3)))))
+p=claripy.Not(claripy.Or(claripy.And(claripy.And(x2>0x63,x0>0x18,x0>0x32,x1==0x3),claripy.And(x2>0x63,x0>0x18,x0>0x32,x1==0x3)),claripy.And(claripy.Not(claripy.And(x2>0x63,x0>0x18,x0>0x32,x1==0x3)),claripy.Not(claripy.And(x2>0x63,x0>0x18,x0>0x32,x1==0x3)))))
 s = claripy.Solver()
 s.add(p)
 print(s.satisfiable())
