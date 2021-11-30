4c5ab6b9f5eba035870f7f7d2286025918509262
laboni68 <labonisarker15@gmail.com>
2021-11-29 17:40:07 -0800

    timing is added

--- finalRun.py
+++ finalRun.py
@@ -4,7 +4,7 @@ x0 = claripy.BVS("x0",64)
 x1 = claripy.BVS("x1",64)
 x2 = claripy.BVS("x2",64)
 
-p=claripy.Not(claripy.Or(claripy.And(claripy.Or(claripy.And(claripy.Not(x1<=0xfffffffffffff000),x2==1),claripy.And(x1<=0xfffffffffffff000)),claripy.Or(claripy.And(claripy.Not(x1<=0xfffffffffffff000),x2==1),claripy.And(x1<=0xfffffffffffff000))),claripy.And(claripy.Not(claripy.Or(claripy.And(claripy.Not(x1<=0xfffffffffffff000),x2==1),claripy.And(x1<=0xfffffffffffff000))),claripy.Not(claripy.Or(claripy.And(claripy.Not(x1<=0xfffffffffffff000),x2==1),claripy.And(x1<=0xfffffffffffff000))))))
+p=claripy.Not(claripy.Or(claripy.And(claripy.Or(),claripy.Or(claripy.And(claripy.Not(x2<=0xfffffffffffff000),x0==1),claripy.And(x2<=0xfffffffffffff000))),claripy.And(claripy.Not(claripy.Or()),claripy.Not(claripy.Or(claripy.And(claripy.Not(x2<=0xfffffffffffff000),x0==1),claripy.And(x2<=0xfffffffffffff000))))))
 s = claripy.Solver()
 s.add(p)
 print(s.satisfiable())
