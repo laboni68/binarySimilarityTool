da9aea0377d43d271e646046f0c5621d4f8ce4ef
laboni68 <labonisarker15@gmail.com>
2022-05-30 20:45:51 -0700

    new test case 17 18 is added

--- finalRun.py
+++ finalRun.py
@@ -4,7 +4,7 @@ x0 = claripy.BVS("x0",64)
 x1 = claripy.BVS("x1",64)
 x2 = claripy.BVS("x2",64)
 
-p=claripy.Not(claripy.Or(claripy.And(claripy.And(x0>0x63,x2>0x19,x1==0x2),claripy.And(x0>0x63,x2<=0x18,x1==0x2)),claripy.And(claripy.Not(claripy.And(x0>0x63,x2>0x19,x1==0x2)),claripy.Not(claripy.And(x0>0x63,x2<=0x18,x1==0x2)))))
+p=claripy.Not(claripy.Or(claripy.And(claripy.And(x1>0x63,x2>0x18,x2>0x32,x0==0x3),claripy.And(x1>0x63,x2>0x33,x0==0x3)),claripy.And(claripy.Not(claripy.And(x1>0x63,x2>0x18,x2>0x32,x0==0x3)),claripy.Not(claripy.And(x1>0x63,x2>0x33,x0==0x3)))))
 s = claripy.Solver()
 s.add(p)
 print(s.satisfiable())
