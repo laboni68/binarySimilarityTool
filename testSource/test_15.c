ed2866db36ca018c83ea2283eb6a0c8f681561de
laboni68 <labonisarker15@gmail.com>
2022-06-01 00:07:37 -0700

    new test case from poster is added

--- testSource/test_15.c
+++ testSource/test_15.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(){
     int buffer[100];
+    printf("Input password: ");
     int i, k=5;
     while(i<=5){
 	i++;
--- testSource/test_16.c
+++ testSource/test_16.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(){
     int buffer[100];
+    printf("Input password: ");
     int k=5;
     for(int i=0;i<=5;i++){
 	k++;
